a3a30871357057b660194856d59575828ba3ddeb
Cnaf254 <133096126+Cnaf254@users.noreply.github.com>
2024-03-23 20:10:45 -0900

    error fixes

--- client/src/axiosConfig.js
+++ client/src/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 const axiosBase = axios.create({
-    baseURL:'http://localhost:5501/api'
+    baseURL:'https://evangadiforum.gezueshetu.com/api'
 })
 export default axiosBase
